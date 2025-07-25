1bf2128f0f2af24f6f29905fe2bde494b2b71684
Coder Joan <codejoan@op.pl>
2025-07-25 23:24:59 +0200

    Add /config

---
+++ config/paths.py
@@ -0,0 +1,4 @@
+MAPS = "resources/maps/"
+BOOKS = "resources/books/book_names.txt"
+TRANSLATIONS = "resources/bibles/translations.json"
+ALTERNATIVE_BOOK_NAMES = "resources/books/alternative_book_names.json"
