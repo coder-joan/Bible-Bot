409e1cdb242795c8d9a27c321673f1ae730415b5
Coder Joan <codejoan@op.pl>
2025-09-25 17:27:52 +0200

    Change file path in BOOKS

--- src/config/paths.py
+++ src/config/paths.py
@@ -1,4 +1,4 @@
 MAPS = "resources/maps/"
-BOOKS = "resources/books/book_names.txt"
+BOOKS = "resources/books/book_names.json"
 TRANSLATIONS = "resources/bibles/translations.json"
 ALTERNATIVE_BOOK_NAMES = "resources/books/alternative_book_names.json"
