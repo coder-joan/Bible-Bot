6aee0acee2513d9f7e9127462968d29a5226d2d8
Coder Joan <codejoan@op.pl>
2025-10-22 23:24:29 +0200

    Deleted src folder

--- src/config/paths.py
+++
@@ -1,4 +0,0 @@
-MAPS = "resources/maps/"
-BOOKS = "resources/books/book_names.json"
-TRANSLATIONS = "resources/bibles/translations.json"
-ALTERNATIVE_BOOK_NAMES = "resources/books/alternative_book_names.json"
--- src/utils/italic_font.py
+++
@@ -1,4 +0,0 @@
-import re
-
-def italic_font(text):
-    return re.sub(r'\[([^\]]+)\]', r'*\1*', text)
