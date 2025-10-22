f63b330e767a700fb86a20776e3c3acdc11bfbd0
Coder Joan <codejoan@op.pl>
2025-10-23 00:07:52 +0200

    Add config

---
+++ config/links.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SERVER_LINK = os.getenv("SERVER_LINK")
+GITHUB_LINK = os.getenv("GITHUB_LINK")
+INVITE_LINK = os.getenv("INVITE_LINK")
+WEBSITE_LINK = os.getenv("WEBSITE_LINK")
+TOP_GG_LINK = os.getenv("TOP_GG_LINK")
+DISCORD_BOT_LIST_LINK = os.getenv("DISCORD_BOT_LIST_LINK")
